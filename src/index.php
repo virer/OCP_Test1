b2031ed8644505567c022f52113da54846b14c38
scaps-nrb <sebastien.caps@nrb.be>
2020-09-07 15:01:50 +0200

    Added index.php

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Hello World</title>
+  </head>
+  <body>
+    <?php if ($_POST['who'] != '') {?>
+      <h1>Hello <?php echo htmlspecialchars($_POST['who']); ?></h1>
+      <a href="index.php">Greet someone else</a>
+    <?php } else { ?>
+      <form class="greetingForm" action="index.php" method="post">
+        <label for="who">Say hello to</label>
+        <input type="text" name="who">
+        <input type="submit" name="greet" value="Say Hello">
+      </form>
+    <?php } ?>
+  <br>
+  <br>
+  Original work: <a href="https://github.com/rmuch/docker-php-hello-world.git">https://github.com/rmuch/docker-php-hello-world.git</a>
+  </body>
+</html>
